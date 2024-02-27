1fd22c493e90fd625efe00da949925de3a302fcc
Danilo Guanabara <danilo@sensorial.systems>
2024-02-26 21:04:17 -0300

    Fixing enums (#18)
    * Fixing enums
    
    * Moving #[allow(dead_code)] to a more obvious place
    
    * Removing dead_code

--- build/codegen/mod.rs
+++ build/codegen/mod.rs
@@ -1,5 +1,5 @@
 use std::{
-    cell::{Cell, RefCell},
+    cell::RefCell,
     collections::HashMap,
 };
 
@@ -9,16 +9,16 @@ pub mod stream;
 pub mod structs;
 
 #[derive(Clone)]
-pub struct UniqueIds(Cell<u32>, RefCell<HashMap<String, u32>>);
+pub struct UniqueIds(RefCell<HashMap<String, u32>>);
 
 impl UniqueIds {
     pub fn new() -> Self {
-        UniqueIds(Cell::new(0), RefCell::new(HashMap::new()))
+        UniqueIds(RefCell::new(HashMap::new()))
     }
 
     pub fn next_names(&self, inputs: &[&str]) -> String {
         let input = inputs.join("_");
-        let mut names = self.1.borrow_mut();
+        let mut names = self.0.borrow_mut();
 
         if let Some(&id) = names.get(&input) {
             names.insert(input.clone(), id + 1);
